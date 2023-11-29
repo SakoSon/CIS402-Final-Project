6d293b442cc40337a79af44007b9f70a6f196ea0
SakoSon <148659886+SakoSon@users.noreply.github.com>
2023-11-29 14:26:44 -0500

    Add files via upload

---
+++ menageriedb.py
@@ -0,0 +1,15 @@
+import mysql.connector as mc
+
+conn = mc.connect(host='localhost', user='root', password='Ss209951839!', db='menagerie')
+c = conn.cursor()
+
+def display_data():
+    c.execute('SELECT name, birth, MONTH(birth) FROM pet')
+    databases = c.fetchall()
+    for database in databases:
+        print(database)
+
+def main():
+    display_data()
+
+main()
